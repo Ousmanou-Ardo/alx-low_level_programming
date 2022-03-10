7fa38addce2c8d6f69cca0cd383a26d4dee6cd09
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-10 01:11:19 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 /**
- * main -entry point function
- * return: 0 if the main is ok gere us sirt
+ * main - entry point function, return: 0 if the main is ok gere us sirt
+ (* a blank line
+ * Description: Longer description of the function)?
+ (* section header: Section description)*
+ * Return: Description of the returned value
  * this a main function that will print
  */
 int main(void)
