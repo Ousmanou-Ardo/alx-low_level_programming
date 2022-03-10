eecf62613a168770d2be51bb0853bc2d2a9549ce
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-10 01:30:22 -0800

    sizof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
-/*this a description of a min */
+/**
+ * main - this a description of a min
+(* a blank line it needs to be a little bit
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
 int main(void)
 {
 /* sizeof evaluates the size of a vari */
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int:%lu byte(s)\n", sizeof(long long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
