cd0199ca6d0e23373445b35f961a60f1ddc4bffe
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-10 01:17:59 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 /**
-*the main fuction is the entry point of the program 
-*/
+ * main - fuction is the entry point of the program,here we are
+(* a blank line 
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ * from this program we can print the string feeded to the printf function
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
