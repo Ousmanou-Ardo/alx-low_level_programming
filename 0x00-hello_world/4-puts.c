a5fd54b0b66094defbace1e5c826ef451ee503a8
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-10 01:07:32 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * @main:entry point function
- * gere us sirt
+ * main -entry point function
+ * return: 0 if the main is ok gere us sirt
  * this a main function that will print
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
