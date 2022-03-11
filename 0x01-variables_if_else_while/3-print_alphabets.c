c275df346f69b0338c664b92e79f531b931fc5eb
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-11 06:47:37 -0500

    print-alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - echoes these alphabets in lowercase, then in uppercase.
+(*
+ * Return: 0 when success occurs
+ */
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+
+}
