d54ff960f5d2d68f2d0602cb0297b5abf8d13420
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-15 05:18:50 -0700

    10_times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabet from a-z 10 times.
+ * Return: Nothing to be returned.
+ * another line here
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int i = 0;
+ 
+for (c = 'a'; c <= 'z'; c++)
+while(i < 10)
+{
+_putchar(c);
+i++
+}
+_putchar('\n');
+}
