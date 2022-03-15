089df25517e520d75cee57999d59db9d4f7ef1c2
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-15 04:34:01 -0700

    mychar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * main - here is the entry point of our function
- * 
- * return: 0 if the everything runs as expected
+ * mana 
+ * return: 0 if the everything runs
  *let's try to write the function
  */
 int main(void)
 {
-char y[] = {'_', 'p', 'u', 't', 'c','h', 'a', 'r'};
+char y[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 int h = 0;
 
 while (y[h] != '\0')
