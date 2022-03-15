ef34bec720adc436bb9a672bc45254c7eabda419
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-15 05:37:36 -0700

    up_too

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - alphabet from a-z 10 times.
+ * print_alphabet_x10 - alphabet from a-z 10 times.
  * Return: Nothing to be returned.
  * another line here
  */
@@ -8,9 +8,9 @@ void print_alphabet_x10(void)
 {
 char c;
  
-for (i = 0; c <= 9; c++)
+for (i = 0; i <= 9; i++)
 {
-while( c <=  'z')
+while (c <=  'z')
 {
 _putchar(c);
 i++;
