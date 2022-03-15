63ea8ec90650b0572905f8170fa89f4d1aec92f9
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-15 05:02:34 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,10 +5,14 @@
  */
 int main(void)
 {
-char y[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};                                                                                                                                   int h = 0;
+char y[] = {'_', 'p', 'u','t', 'c', 'h',
+	    'a', 'r'};
+int h = 0;
 while (y[h] != '\0')
 {
 _putchar(y[h]);
+
+h++;
 }
 _putchar('\n');
 return (0);
