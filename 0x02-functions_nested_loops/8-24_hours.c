04bdf883eb78cf9b97a15d3ebc45493229561412
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-15 07:14:14 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,8 +11,8 @@ for (n = 0; n < 24; n++)
 {
 for (m = 0; m < 60; m++)
 {
-_putchar((h / 10) + '0');
-_putchar((h % 10) + '0');
+_putchar((n/ 10) + '0');
+_putchar((n % 10) + '0');
 _putchar(':');
 _putchar((m / 10) + '0');
 _putchar((m % 10) + '0');
