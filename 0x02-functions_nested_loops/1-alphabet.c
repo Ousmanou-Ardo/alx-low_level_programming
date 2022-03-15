93511cfcd48117629fdc8165ec2a7a4b723392f3
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-15 01:06:06 -0700

    alpha_lower_case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+include "main.h"
+/**
+ * main - our entry point function
+(* from there we can write all the code
+ * we want and call the user_define function
+ * return: 0 if the code exectued successfully
+ */
+ int main(void)
+{
+  char al='a';
+ 
+
+
+  while (al <= 'z')
+    {
+      _putchar(al);
+      al++;
+    }
+  _putchar('\n');
+  return (0);
+}
