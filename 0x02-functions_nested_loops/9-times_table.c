99c174c324b279bc9f52b1d7e3340cf101b29545
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-15 07:32:07 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,25 +1,33 @@
 #include "main.h"
 /**
  * times_table - echoe the numbers m 0 - 9. 
-(*
+(* biba
  * Return: Nothing. 
 */
 void times_table(void)
 { int k, j, res;
 for (k = 0; k <= 9; k++)
-{ for (j = 0; j <= 9; j++)
-{ res = (k * j);
-if (j != 0) { _putchar(','); _putchar(' ');
+{
+for (j = 0; j <= 9; j++)
+{
+res = (k * j);
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
 }
 if (res >= 10)
-{ _putchar((res / 10) + '0');
+{
+_putchar((res / 10) + '0');
 _putchar((res % 10) + '0');
 }
 else if (res < 10 && j != 0)
-{ _putchar(' ');
+{
+_putchar(' ');
 _putchar((res % 10) + '0');
 }
-else _putchar((res % 10) + '0');
+else 
+_putchar((res % 10) + '0');
 }
 _putchar('\n');
 }
