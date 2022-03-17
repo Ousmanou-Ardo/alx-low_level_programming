787bbfc3fb020e7b7c31d709eaa2fbdcdf51c496
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-17 02:08:16 -0400

    testing main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests  that can check if integer is positive or negative
+* Return: the value returhed is 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
