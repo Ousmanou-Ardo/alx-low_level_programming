ff154f1470cc7b787a6a611fd5da0a420b8f0203
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-17 02:35:23 -0400

    modified

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,7 +9,10 @@ int main(void)
 {
         int i;
 
-        if (i != 0)
+        if (i = 0)
+        {
+            printf("%d is zero", i);
+        }
         positive_or_negative(i);
 
         return (0);
