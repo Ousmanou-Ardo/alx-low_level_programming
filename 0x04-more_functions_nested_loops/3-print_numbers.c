f1ec915aa823493aa88411fb3f679fef1554f940
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-17 13:56:40 -0400

    3-mul

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_numbers - output digits from 1-9
+ * here the is no return.
+ */
+void print_numbers(void)
+{
+int p = 0;
+
+while (p <= 9)
+{
+_putchar(p + '0');
+++p;
+}
+_putchar('\n');
+}
