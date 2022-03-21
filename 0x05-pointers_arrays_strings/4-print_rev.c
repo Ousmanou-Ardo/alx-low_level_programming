5b1a15dc71d65acaab1ba52af943e9df689e3ab8
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-21 15:22:35 -0400

    print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,18 +3,17 @@
  * print_rev - output string, then a new line.
  * @s: the input variable.
  */
-void print_rev(char *s)
-{
-int q;
-
-while (s[q] != '\0')
-{
-q++;
-}
-q--;
-while (s[q] != '\0')
-{
-_putchar(s[q--]);
-}
-_putchar('\n');
+void print_rev(char *s)                                                                                                                             
+{                                                                                                                                                   
+        int i = 0;                                                                                                                                  
+                                                                                                                                                    
+        while (*(s + i) != '\0')                                                                                                                    
+                i += 1;                                                                                                                             
+        i -= 1;                                                                                                                                     
+        while (i >= 0)                                                                                                                              
+        {                                                                                                                                           
+                _putchar(*(s + i));                                                                                                                 
+                i--;                                                                                                                                
+        }                                                                                                                                           
+        _putchar('\n');                                                                                                                             
 }
