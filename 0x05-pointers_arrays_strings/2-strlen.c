78d01daf48450d4f7151dc4f54641b4bb3915391
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-21 08:48:24 -0400

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - compute length of a string.
+ * @str: pointeer of our character .
+ * Return: length of the string is returned;
+ */
+int _strlen(char *str)
+{
+int e;
+e = 0;
+while (*str != '\0')
+{
+e++;
+str++;
+}
+return (i);
+}
