9689499d13998e133119ed7d6d554016049098e4
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-21 09:16:53 -0400

    put

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,12 +3,12 @@
  * _puts - output string, then a new line.
  * @str: string to be printed.
  */
-void _puts(char *str)
+void _puts(char *s)
 {
-while (*str != '\0')
+while (*s != '\0')
 {
-_putchar(*str);
-str++;
+_putchar(*s);
+s++;
 }
 _putchar('\n');
 }
