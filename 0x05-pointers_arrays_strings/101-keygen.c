dd000af5ded8eea29aeb539eca0a62a44ab5e63c
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-22 02:43:59 -0400

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,28 @@
 #import "main.h"
 /**
  * main - entry point of the function
- * Return:
+ *
+ * @ac: Arguments counter
+ * @av: Arguments array
+ * Return: 1 on error, 0 otherwise
  */
-int main(void)
+int main(int ac, char **av)
 {
+	unsigned long int c;
+
+	if (ac != 2)
+	{
+		printf("Usage: %s password\n", av[0]);
+		return (1);
+	}
+	c = checksum(av[1]);
+	/* printf("%lu\n", c); */
+	/* "Talk is cheap. Show me the code." */
+	if (c != 2772)
+	{
+		printf("Wrong password\n");
+		return (1);
+	}
+	printf("Tada! Congrats\n");
 	return (0);
 }
