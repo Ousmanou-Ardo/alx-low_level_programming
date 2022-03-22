a42162651004619e93a3b676015409246f98a830
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-22 02:30:27 -0400

    print-array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,12 @@ void print_array(int *a, int n)
 int i;
 
 i = 0;
-while (i < n)
+
+for (; i < n; i++)
 {
-if (i != n - 1)
-printf("%d, ", a[i]);
-else
 printf("%d", a[i]);
-i++;
+if (i < n - 1)
+printf(", ");
 }
 _putchar('\n');
 }
