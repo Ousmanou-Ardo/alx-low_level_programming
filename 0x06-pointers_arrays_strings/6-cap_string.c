4306b46b2222ca4903cb5c1b21d2114c2d13d37a
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-03-24 08:58:40 -0400

    cap-string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,42 +1,22 @@
 /**
- * cap_sing - capitalizes string's words.
+ * cap_string - capitalizes string's words.
  * @s: pointing to input string.
- * Return: Returning pointer to the capitalized string.
+ * Return: pointer to the capitalized string.
  */
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= ('a' - 'A');
 	while (s[i] != '\0')
 	{
-		/*for (k = 0; sep[k] != '\0';k++)
-			if (s[i - 1] == sep[k] && (s[i] >= 'a' && s[i] <= 'z'))
+		for (j = 0; sep[j] != '\0'; j++)
+			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
 				s[i] -= ('a' - 'A');
-		i++;*/
-        while (!(s[i] >= 'a' && s[i] <= 'z'))
-			i++;
-
-		if (s[i - 1] == ' ' ||
-		    s[i - 1] == '\t' ||
-		    s[i - 1] == '\n' ||
-		    s[i - 1] == ',' ||
-		    s[i - 1] == ';' ||
-		    s[i - 1] == '.' ||
-		    s[i - 1] == '!' ||
-		    s[i - 1] == '?' ||
-		    s[i - 1] == '"' ||
-		    s[i - 1] == '(' ||
-		    s[i - 1] == ')' ||
-		    s[i - 1] == '{' ||
-		    s[i - 1] == '}' ||
-		    i == 0)
-			s[i] -= 32;
-
 		i++;
-
-
-
-
-
 	}
 	return (s);
 }
