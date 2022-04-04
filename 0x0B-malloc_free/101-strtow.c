7e5cb1400452dc25fbb46ed61e1aedae363d4815
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-04-04 16:51:25 -0400

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * wordcounter - counts words and the letters in them
+ * wordcounter - counts words and the alphabet in them
  * @str: string to count
  * @pos: position of the word to count characters from
  * @firstchar: position of the first letter of the word
